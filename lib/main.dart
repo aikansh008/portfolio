896dff53d30a7ed3609839ed54cb274ac5f59250
aikansh008 <132533740+aikansh008@users.noreply.github.com>
2023-10-08 21:02:24 +0530

    Finish Portfolio

--- lib/main.dart
+++ lib/main.dart
@@ -13,25 +13,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 84, 11, 89)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'My portfoliio'),
+      home: const MyHomePage( 
+        title: 'MY PORTFOLIO', 
+      ),
+     
       debugShowCheckedModeBanner: false,
     );
   }
@@ -40,15 +29,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+ 
   final String title;
 
   @override
@@ -60,11 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      
       _counter++;
     });
   }
@@ -78,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-        backgroundColor: Color.fromARGB(205, 31, 231, 231),
+        backgroundColor: Color.fromARGB(134, 175, 50, 194),
         appBar: AppBar(
           // TRY THIS: Try changing the color here to a specific color (to
           // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
@@ -87,6 +64,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
+          centerTitle: true,
+          titleSpacing: 3,
         ),
         body: Container(
           width: 1500,
@@ -95,49 +74,266 @@ class _MyHomePageState extends State<MyHomePage> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  CircleAvatar(
-                    radius: 100,
-                  ),
-                  Text(
-                    " This Is AIKANSH TIWARI",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 27,
-                      fontWeight: FontWeight.w800,
-                      color: Colors.black,
-                      letterSpacing: 2.0,
+                  Padding(
+                    padding: const EdgeInsets.only( top : 8.0),
+                    child: CircleAvatar(
+                      radius: 100,
+                      backgroundImage: AssetImage('assets/Images/IMG-20230624-WA0000.jpg'),
                     ),
                   ),
-                  Text(
-                    'About Me',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.pinkAccent,
-                      fontWeight: FontWeight.w700,
-                      letterSpacing: 2.0,
+                  Padding(
+                    padding: const EdgeInsets.only( top:8.0),
+                    child: Text(
+                      " This Is AIKANSH TIWARI",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 27,
+                        fontWeight: FontWeight.w800,
+                        color:Color.fromARGB(255, 81, 215, 197),
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:15.0),
+                    child: Text(
+                      'About Me',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Color.fromARGB(222, 50, 203, 15),
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 2.0,
+                      ),
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.all(20.0),
-                    child: Text(
-                      "Hi,I'm Aikansh Tiwari,an app developer passionate about crafting remarkable digital experiences.I'm also intrested in backend development and love tackling challenges.Plus,I'm a DSA enthusiast and a competitive programmer,always aiming for excellence.Let's innovate together!",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white,
+                    child: Container(
+                      child: Text(
+                        "Hi,I'm Aikansh Tiwari,an app developer passionate about crafting remarkable digital experiences.I'm also intrested in backend development and love tackling challenges.Plus,I'm a DSA enthusiast and a competitive programmer,always aiming for excellence.Let's innovate together!",
+                        style: TextStyle(
+                          fontFamily:'fontmain',
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
                     ),
                   ),
-                  Text(
-                    "SKILLS",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w800,
-                      color: Colors.purpleAccent,
+                  Padding(
+                    padding: const EdgeInsets.only( top :20.0,),
+                    child: Text(
+                      "Skills",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w800,
+                        color: Color.fromARGB(255, 214, 141, 81),
+                      ),
                     ),
-                  )
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 40.0, left: 10.0),
+                        child: Image.asset(
+                          "assets/Images/c.png",
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20.0, top: 40.0),
+                        child: Text(
+                          "C language",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 30,
+                            color: Color.fromARGB(255, 51, 182, 215),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0, top: 40.0),
+                        child: Image.asset(
+                          "assets/Images/ISO_C++_Logo.svg.png",
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0, top: 40.0),
+                        child: Text(
+                          "C++",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 30,
+                            color: Color.fromARGB(255, 51, 182, 215),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0, top: 40.0),
+                        child: Image.asset(
+                          "assets/Images/html-5.png",
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0, top: 40.0),
+                        child: Text(
+                          "HTML",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 30,
+                            color: Color.fromARGB(255, 51, 182, 215),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0, top: 40.0),
+                        child: Image.asset(
+                          "assets/Images/css-3.png",
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0, top: 40.0),
+                        child: Text(
+                          "CSS",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 30,
+                            color: Color.fromARGB(255, 51, 182, 215),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0, top: 40.0),
+                        child: Image.asset(
+                          "assets/Images/icons8-flutter-96.png",
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0, top: 40.0),
+                        child: Text(
+                          "FLUTTER",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 30,
+                            color: Color.fromARGB(255, 51, 182, 215),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40.0, bottom: 20),
+                    child: Text(
+                      "Connect With Me",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                        color: Color.fromARGB(255, 206, 177, 47),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 50,
+                    shadowColor:Color.fromARGB(255, 126, 223, 47),
+                    color: Color.fromARGB(255, 197, 179, 179),
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 25.0,
+                      
+                      
+                    ),
+                    
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.black,
+                      ),
+                      title: Text("aikanshtiwari007@gmail.com",style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                    ),
+                  ),
+                   Card(
+                    color: Color.fromARGB(255, 197, 179, 179),
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 25.0,
+                      
+                    ),
+                    
+                    child: ListTile(
+                      leading:Image.asset("assets/Images/icons8-phone-50.png"),
+                       title: Text("+91 6387175488",style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                    ),
+                  ),
+                   Card(
+                    color: Color.fromARGB(255, 197, 179, 179),
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 25.0,
+                      
+                    ),
+                    
+                    child: ListTile(
+                      leading: Image.asset("assets/Images/icons8-linkedin-48.png"),
+                       title: Text("Aikansh Tiwari",style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                    ),
+                  ),
+                   Card(
+                    color: Color.fromARGB(255, 197, 179, 179),
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 25.0,
+                      
+                    ),
+                    
+                    child: ListTile(
+                      leading: Image.asset("assets/Images/icons8-instagram-50.png"),
+                       title: Text("aikansh_tiwari13",style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                    ),
+                  ),
                 ]),
           ),
         ));
